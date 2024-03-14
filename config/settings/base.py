147f9757eab5c6ca34a05317dbb1c6d745b4f9c4
GRSdeteccion <grsdeteccionmkt@gmail.com>
2024-03-13 19:55:04 -0600

    test

--- config/settings/base.py
+++ config/settings/base.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 10
 }
 
 SPECTACULAR_SETTINGS = {
